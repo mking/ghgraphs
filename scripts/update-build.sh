48db7352dafd779c9e31c0b28170c1de1562f71d
Matthew King <mking@users.noreply.github.com>
2015-08-10 08:02:01 -0700

    update script

--- scripts/update-build.sh
+++ scripts/update-build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-git checkout gh-pages && git rebase master && webpack && git add -f ./build/app.js && git commit -am 'update build' && git push -f && git checkout master
+git checkout gh-pages && git rebase master && webpack && git add -f ./build/app.js && git commit -am 'update build' && echo 'updated'
+# && git push -f && git checkout master
