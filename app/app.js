7ac1149fb98bc96d9dcec423cd46111f339337f5
Matthew King <mking@users.noreply.github.com>
2015-08-10 08:12:00 -0700

    fix async

--- app/app.js
+++ app/app.js
@@ -1,16 +1,8 @@
 import './app.scss';
-import Promise from 'bluebird';
 import React from 'react';
 import RepoCommits from './RepoCommits';
 
-const jsonAsync = Promise.promisify(d3.json);
-
-jsonAsync('./json/contributors.json').then(contributors => {
-  console.log(contributors);
-
-  React.render(
-    <RepoCommits
-      contributors={contributors}/>,
-    document.getElementById('content')
-  );
-});
+React.render(
+  <RepoCommits/>,
+  document.getElementById('content')
+);
